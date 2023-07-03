bb8425e616bf5a87ac1488ad8ddc82fa52bdaa69
akshhaygrao77 <akshaygrao77@gmail.com>
2023-07-03 16:03:02 +0530

    Added feature to plot DFT of jacobians too

--- frequency_spectrum_profiler.py
+++ frequency_spectrum_profiler.py
@@ -24,6 +24,7 @@ from cleverhans.torch.attacks.fast_gradient_method import fast_gradient_method
 from cleverhans.torch.attacks.projected_gradient_descent import (
     projected_gradient_descent,
 )
+
 from keras.datasets import mnist, fashion_mnist
 
 from conv4_models import get_model_instance, get_model_instance_from_dataset
@@ -57,6 +58,10 @@ def generate_mean_jacobian_fft(dataloader,model):
     criterion = nn.CrossEntropyLoss().to(device)
     
     all_ffts = []
+    all_corr_ffts = []
+    all_incorr_ffts = []
+    jac_corr_ffts = []
+    jac_incorr_ffts = []
     all_power_ffts = []
     loader = tqdm.tqdm(dataloader, desc='Generating jacobian FFT')
     for _, data in enumerate(loader, 0):
@@ -67,21 +72,101 @@ def generate_mean_jacobian_fft(dataloader,model):
 
         # forward + backward + optimize
         outputs = model(inputs)
+        
         loss = criterion(outputs, labels)
         loss.backward()
-
+        _, predicted = torch.max(outputs.data, 1)
+        
         input_grad = inputs.grad
         input_grad = torch.fft.fft2(input_grad)
         input_grad = torch.abs(torch.fft.fftshift(input_grad))
         all_ffts.append(input_grad)
         all_power_ffts.append(input_grad*input_grad)
+
+        with torch.no_grad():
+            correct_indices = torch.squeeze((predicted==labels).nonzero(),1)
+            corr_inputs = torch.index_select(inputs, dim=0, index=correct_indices)
+        if(corr_inputs.size()[0] != 0):
+            corr_labels = torch.index_select(labels, dim=0, index=correct_indices)
+            corr_inputs.requires_grad_()
+            outputs = model(corr_inputs)
+            loss = criterion(outputs, corr_labels)
+            loss.backward()
+            
+            input_grad = corr_inputs.grad
+            input_grad = torch.fft.fft2(input_grad)
+            input_grad = torch.abs(torch.fft.fftshift(input_grad))
+            jac_corr_ffts.append(input_grad)
+
+            corr_inputs = torch.fft.fft2(corr_inputs)
+            corr_inputs = torch.abs(torch.fft.fftshift(corr_inputs))
+            all_corr_ffts.append(corr_inputs)
+
+        with torch.no_grad():
+            incorrect_indices = torch.squeeze((predicted != labels).nonzero(),1)
+            incorr_inputs = torch.index_select(inputs, dim=0, index=incorrect_indices)
+        if(incorr_inputs.size()[0] != 0):
+            incorr_labels = torch.index_select(labels, dim=0, index=incorrect_indices)
+            incorr_inputs.requires_grad_()
+            outputs = model(incorr_inputs)
+            loss = criterion(outputs, incorr_labels)
+            loss.backward()
+            
+            input_grad = incorr_inputs.grad
+            input_grad = torch.fft.fft2(input_grad)
+            input_grad = torch.abs(torch.fft.fftshift(input_grad))
+            jac_incorr_ffts.append(input_grad)
+
+            incorr_inputs = torch.fft.fft2(incorr_inputs)
+            incorr_inputs = torch.abs(torch.fft.fftshift(incorr_inputs))
+            all_incorr_ffts.append(incorr_inputs)
+    
+    all_corr_ffts = torch.cat(all_corr_ffts,dim=0)
+    all_corr_ffts = torch.mean(all_corr_ffts,dim=(0,1))
+
+    all_incorr_ffts = torch.cat(all_incorr_ffts,dim=0)
+    all_incorr_ffts = torch.mean(all_incorr_ffts,dim=(0,1))
+
+    jac_corr_ffts = torch.cat(jac_corr_ffts,dim=0)
+    jac_corr_ffts = torch.mean(jac_corr_ffts,dim=(0,1))
+
+    jac_incorr_ffts = torch.cat(jac_incorr_ffts,dim=0)
+    jac_incorr_ffts = torch.mean(jac_incorr_ffts,dim=(0,1))
+
+    all_ffts = torch.cat(all_ffts,dim=0)
+    all_ffts = torch.mean(all_ffts,dim=(0,1))
+
+    all_power_ffts = torch.cat(all_power_ffts,dim=0)
+    all_power_ffts = torch.mean(all_power_ffts,dim=(0,1))
+    return all_ffts,all_power_ffts,all_corr_ffts,all_incorr_ffts,jac_corr_ffts,jac_incorr_ffts
+
+def generate_diff_mean_fft(dataloader1,dataloader2):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    all_ffts = []
+    all_power_ffts = []
+    iter_dataloader2 = enumerate(dataloader2)
+    loader = tqdm.tqdm(dataloader1, desc='Generating perturbation FFT')
+    for ind, data in enumerate(loader, 0):
+        inputs, _ = data
+        inputs = inputs.to(
+            device).type(torch.float64)
+        _,(inputs2,_) = next(iter_dataloader2)
+        inputs2 = inputs2.to(
+            device).type(torch.float64)
+        assert inputs2.size()==inputs.size(), "Not same sizes:{} vs {} at ind:{}".format(inputs.size(),inputs2.size(),ind)
+        diff = inputs2 - inputs
+
+        diff = torch.fft.fft2(diff)
+        diff = torch.abs(torch.fft.fftshift(diff))
+        all_ffts.append(diff)
+        all_power_ffts.append(diff*diff)
     
     all_ffts = torch.cat(all_ffts,dim=0)
     all_ffts = torch.mean(all_ffts,dim=(0,1))
     all_power_ffts = torch.cat(all_power_ffts,dim=0)
     all_power_ffts = torch.mean(all_power_ffts,dim=(0,1))
     return all_ffts,all_power_ffts
-
 
 def generate_mean_fft(dataloader):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -92,7 +177,7 @@ def generate_mean_fft(dataloader):
     for _, data in enumerate(loader, 0):
         inputs, _ = data
         inputs = inputs.to(
-            device)
+            device).type(torch.float64)
 
         inputs = torch.fft.fft2(inputs)
         inputs = torch.abs(torch.fft.fftshift(inputs))
@@ -110,22 +195,26 @@ def output_PIL_image(images,path):
         images, unnormalize=False)
     save_image(std01_vis_images,path)
 
-def output_plt_image(images,path):
+def output_plt_image(images,path,title=""):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    cax = ax.imshow(images.cpu().numpy(), interpolation='nearest')
+    ax.set_xticks([])
+    ax.set_yticks([])
+    cax = ax.imshow(images.detach().cpu().numpy(), interpolation='nearest')
+    ax.set_title(title)
     cbar = fig.colorbar(cax)
-    fig.savefig(path)
+    fig.tight_layout()
+    fig.savefig(path,bbox_inches='tight')
 
 if __name__ == '__main__':
     # fashion_mnist , mnist, cifar10
-    dataset = 'mnist'
+    dataset = 'fashion_mnist'
     # conv4_dlgn , plain_pure_conv4_dnn , conv4_dlgn_n16_small , plain_pure_conv4_dnn_n16_small , conv4_deep_gated_net , conv4_deep_gated_net_n16_small
     # fc_dnn , fc_dlgn , fc_dgn , dlgn__conv4_dlgn_pad_k_1_st1_bn_wo_bias__
-    model_arch_type = 'dlgn__conv4_dlgn_pad_k_1_st1_bn_wo_bias__'
+    model_arch_type = 'plain_pure_conv4_dnn'
     batch_size = 64
 
-    is_analysis_on_train = True
+    is_analysis_on_train = False
 
     torch_seed = 2022
 
@@ -142,7 +231,7 @@ if __name__ == '__main__':
     wandb_config_additional_dict = None
     # wandb_config_additional_dict = {"type_of_APR": "APRS"}
 
-    direct_model_path = "root/model/save/mnist/adversarial_training/MT_dlgn__conv4_dlgn_pad_k_1_st1_bn_wo_bias___ET_ADV_TRAINING/ST_2022/fast_adv_attack_type_PGD/adv_type_PGD/EPS_0.06/batch_size_128/eps_stp_size_0.06/adv_steps_80/adv_model_dir.pt"
+    direct_model_path = "root/model/save/fashion_mnist/CLEAN_TRAINING/ST_2022/plain_pure_conv4_dnn_dir.pt"
 
     custom_dataset_path = None
     # custom_dataset_path = "data/custom_datasets/freq_band_dataset/mnist__LB.npy"
@@ -241,7 +330,7 @@ if __name__ == '__main__':
             "_PCA_K"+str(number_of_components_for_pca) + \
             "_P_"+str(pca_exp_percent)
 
-    if('CLEAN' in direct_model_path or 'APR_TRAINING' in direct_model_path):
+    if('CLEAN' in direct_model_path or 'APR_TRAINING' in direct_model_path or 'adv_model_dir_epoch' in direct_model_path):
         data_save_prefix = direct_model_path[0:direct_model_path.rfind(
             ".pt")]
     else:
@@ -273,7 +362,7 @@ if __name__ == '__main__':
         adv_target = None
         eps_step_size = 0.06
         eps = 0.06
-        is_adv_attack_on_train = False
+        is_adv_attack_on_train = is_analysis_on_train
 
         final_adv_postfix_for_save = "/RAW_ADV_SAVES/adv_type_{}/EPS_{}/eps_stp_size_{}/adv_steps_{}/on_train_{}/".format(
                 adv_attack_type, eps, eps_step_size, number_of_adversarial_optimization_steps, is_adv_attack_on_train)
@@ -293,66 +382,84 @@ if __name__ == '__main__':
         else:
             print("adv_save_path:", adv_save_path)
             adv_dataset = generate_adv_examples(
-                eval_loader, net, eps, adv_attack_type, number_of_adversarial_optimization_steps, eps_step_size, adv_target, is_save_adv=False, save_path=adv_save_path)
+                eval_loader, net, eps, adv_attack_type, number_of_adversarial_optimization_steps, eps_step_size, adv_target, is_save_adv=True, save_path=adv_save_path)
         
         to_be_analysed_adversarial_dataloader = torch.utils.data.DataLoader(
-                        adv_dataset, shuffle=False, batch_size=128)
+                        adv_dataset, shuffle=False, batch_size=batch_size)
         true_tobe_analysed_dataset_per_class = true_segregation(
                         to_be_analysed_adversarial_dataloader, num_classes_trained_on)
     
     true_eval_dataset_per_class = true_segregation(
         eval_loader, num_classes_trained_on)
-    if(is_analyse_adv):
-        true_tobe_analysed_dataset_per_class = true_segregation(
-                            to_be_analysed_adversarial_dataloader, num_classes_trained_on)
     
     final_data_save_postfix = "/FOURIER_ANALYSIS/ON_TRAIN_{}/dataset_{}/allclasses/".format(
-                    dataset,is_analysis_on_train)
+                    is_analysis_on_train,dataset)
     save_folder = data_save_prefix + final_data_save_postfix
     if not os.path.exists(save_folder):
         os.makedirs(save_folder)
     
+    print("Will be saved in folder:"+save_folder)
+    
     mean_dft,power_dft = generate_mean_fft(eval_loader)
     # output_PIL_image(mean_dft,save_folder +'/orig_mean_DFT.jpg')
-    output_plt_image(mean_dft,save_folder+'/orig_mean_DFT.jpeg')
+    output_plt_image(mean_dft,save_folder+'/orig_mean_DFT.jpeg',"DFT Org")
 
     # power_dft = torch.log(1+power_dft)
     # output_plt_image(power_dft,save_folder+'/log_orig_power_DFT.jpeg')
 
     mean_dft = torch.log(1+mean_dft)
     # output_PIL_image(mean_dft,save_folder +'log_orig_mean_DFT.jpg')
-    output_plt_image(mean_dft,save_folder+'/log_orig_mean_DFT.jpeg')
+    output_plt_image(mean_dft,save_folder+'/log_orig_mean_DFT.jpeg',"Log DFT Org")
 
-    jacobian_mean_dft,jacobian_power_dft = generate_mean_jacobian_fft(eval_loader,net)
+    jacobian_mean_dft,jacobian_power_dft,corr_mean_dft,incorr_mean_dft,jac_cor_dft,jac_incor_dft = generate_mean_jacobian_fft(eval_loader,net)
+    corr_mean_dft = torch.log(1+corr_mean_dft)
+    incorr_mean_dft = torch.log(1+incorr_mean_dft)
+    output_plt_image(corr_mean_dft,save_folder+'/log_orig_corr_mean_DFT.jpeg',"Log DFT Corr Org")
+    output_plt_image(incorr_mean_dft,save_folder+'/log_orig_incorr_mean_DFT.jpeg',"Log DFT Incorr Org")
+
+    jac_cor_dft = torch.log(1+jac_cor_dft)
+    jac_incor_dft = torch.log(1+jac_incor_dft)
+    output_plt_image(jac_cor_dft,save_folder+'/log_jac_corr_mean_DFT.jpeg',"Log DFT Jac ov Corr Org")
+    output_plt_image(jac_incor_dft,save_folder+'/log_jac_incor_mean_DFT.jpeg',"Log DFT Jac ov Incorr Org")
     # jacobian_power_dft = torch.log(1+jacobian_power_dft)
-    # output_PIL_image(jacobian_mean_dft,save_folder +'jacobian_mean_DFT.jpg')
-    output_plt_image(jacobian_mean_dft,save_folder+'jacobian_mean_DFT.jpeg')
+    output_plt_image(jacobian_mean_dft,save_folder+'jacobian_mean_DFT.jpeg',"DFT Jac ov Org")
     # output_plt_image(jacobian_power_dft,save_folder+'log_jacobian_power_DFT.jpeg')
 
-    # jacobian_mean_dft = torch.log(1+jacobian_mean_dft)
-    # output_PIL_image(jacobian_mean_dft,save_folder +'log_jacobian_mean_DFT.jpg')
-    output_plt_image(jacobian_mean_dft,save_folder+'log_jacobian_mean_DFT.jpeg')
+    jacobian_mean_dft = torch.log(1+jacobian_mean_dft)
+    output_plt_image(jacobian_mean_dft,save_folder+'log_jacobian_mean_DFT.jpeg',"Log DFT Jac ov Org")
 
     if(is_analyse_adv):
         mean_dft,power_dft = generate_mean_fft(to_be_analysed_adversarial_dataloader)
         # power_dft = torch.log(1+power_dft)
-        # output_PIL_image(mean_dft,save_folder +'adv_mean_DFT.jpg')
-        output_plt_image(mean_dft,save_folder+'adv_mean_DFT.jpeg')
+        output_plt_image(mean_dft,save_folder+'adv_mean_DFT.jpeg',"DFT Adv")
         # output_plt_image(power_dft,save_folder+'adv_log_power_DFT.jpeg')
 
         mean_dft = torch.log(1+mean_dft)
         # output_PIL_image(mean_dft,save_folder +'adv_log_mean_DFT.jpg')
-        output_plt_image(mean_dft,save_folder+'adv_log_mean_DFT.jpeg')
+        output_plt_image(mean_dft,save_folder+'adv_log_mean_DFT.jpeg',"Log DFT Adv")
 
-        jacobian_mean_dft,jacobian_power_dft = generate_mean_jacobian_fft(to_be_analysed_adversarial_dataloader,net)
+        mean_dft,power_dft = generate_diff_mean_fft(eval_loader,to_be_analysed_adversarial_dataloader)
+        output_plt_image(mean_dft,save_folder+'pert_mean_DFT.jpeg',"DFT perturbation")
+
+        mean_dft = torch.log(1+mean_dft)
+        output_plt_image(mean_dft,save_folder+'pert_log_mean_DFT.jpeg',"Log DFT Perturbation")
+
+
+        jacobian_mean_dft,jacobian_power_dft,corr_mean_dft,incorr_mean_dft,jac_cor_dft,jac_incor_dft = generate_mean_jacobian_fft(to_be_analysed_adversarial_dataloader,net)
+        corr_mean_dft = torch.log(1+corr_mean_dft)
+        incorr_mean_dft = torch.log(1+incorr_mean_dft)
+        output_plt_image(corr_mean_dft,save_folder+'adv_corr_log_mean_DFT.jpeg',"Log DFT Corr Adv")
+        output_plt_image(incorr_mean_dft,save_folder+'adv_incorr_log_mean_DFT.jpeg',"Log DFT Incorr Adv")
         # jacobian_power_dft = torch.log(1+jacobian_power_dft)
-        # output_PIL_image(jacobian_mean_dft,save_folder +'adv_jacobian_mean_DFT.jpg')
-        output_plt_image(jacobian_mean_dft,save_folder+'adv_jacobian_mean_DFT.jpeg')
+        output_plt_image(jacobian_mean_dft,save_folder+'adv_jacobian_mean_DFT.jpeg',"DFT Jac ov Adv")
         # output_plt_image(jacobian_power_dft,save_folder+'adv_log_jacobian_power_DFT.jpeg')
 
         jacobian_mean_dft = torch.log(1+jacobian_mean_dft)
-        # output_PIL_image(jacobian_mean_dft,save_folder +'adv_log_jacobian_mean_DFT.jpg')
-        output_plt_image(jacobian_mean_dft,save_folder+'adv_log_jacobian_mean_DFT.jpeg')
+        jac_cor_dft = torch.log(1+jac_cor_dft)
+        jac_incor_dft = torch.log(1+jac_incor_dft)
+        output_plt_image(jacobian_mean_dft,save_folder+'adv_log_jacobian_mean_DFT.jpeg',"Log DFT Jac ov Adv")
+        output_plt_image(jac_cor_dft,save_folder+'/adv_log_jac_corr_mean_DFT.jpeg',"Log DFT Jac ov Corr Adv")
+        output_plt_image(jac_incor_dft,save_folder+'/adv_log_jac_incorr_mean_DFT.jpeg',"Log DFT Jac ov Incorr Adv")
 
 
     for c_indx in class_indx_to_visualize:
